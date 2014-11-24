7bbc3ae5a49ed7cfe6f6b1b5e8bb10f083d1e660
tca <theconartist@gmail.com>
2014-11-24 04:44:20 -0600

    verify more

--- autoref.scm
+++ autoref.scm
@@ -1,5 +1,5 @@
 (module autoref ()
-(import chicken scheme) (use extras)
+(import chicken scheme) (use extras srfi-1)
 (import pat)
 
 ;; An AUTOREF program has the following syntax
@@ -26,14 +26,30 @@
   (match t
     (`(begin . ,exps) => (begin (print "verifying begin")
                                 (for-each verify-exp exps)))
+    (`(if ,p ,t ,e) => (begin (print "verifying if")
+                              (for-each verify-exp (list p t e))))
+    (`(,f . ,args) => (begin (print "verifying application")
+                             (for-each verify-exp t)))
     (else (if (or (symbol? t) (atomic? t))
               (print "OK!")
               (error (list "Invalid exp!" t))))))
 
+(define (verify-def d)
+  (print "verifying definition")
+  (match d
+    (`(define (,name . ,args) ,body) =>
+     (begin (unless (every symbol? (cons name args))
+              (error (list "Invalid definition!" d)))
+            (verify-exp body)))
+    (else (error(list "Invalid exp!" d)))))
+
+(define (verify-program p)
+  (for-each verify-def p))
+
 (define (main)
   (let ((file-name (car (command-line-arguments))))
     (let ((content (read-file file-name)))
-      (for-each verify-exp content))))
+      (verify-program content))))
 
 (main)
 
